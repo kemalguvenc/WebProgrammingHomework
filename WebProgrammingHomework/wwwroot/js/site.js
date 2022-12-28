1575925da845300e772273037e23fef10b7bb214
Muhammet Kemal Güvenç <kemal.guvenc@ogr.sakarya.edu.tr>
2022-12-28 16:36:04 +0300

    Proje ayarlaması.

--- WebProgrammingHomework/wwwroot/js/site.js
+++ WebProgrammingHomework/wwwroot/js/site.js
@@ -1,9 +1,4 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-
-/*!
+﻿/*!
 * Start Bootstrap - Agency v7.0.11 (https://startbootstrap.com/theme/agency)
 * Copyright 2013-2022 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
